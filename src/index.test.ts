775a04c859fe7fc185be87a85598fbc27046bf2b
Issei Horie <issei.horie@nulab-inc.com>
2019-01-02 11:04:09 +0900

    Added files

---
+++ src/index.test.ts
@@ -0,0 +1,18 @@
+// declare const global: any
+
+describe('typetalk()', () => {
+    // beforeEach(() => {
+    //     global.warn = jest.fn()
+    //     global.message = jest.fn()
+    // })
+    // afterEach(() => {
+    //     global.danger = undefined
+    //     global.warn = undefined
+    //     global.message = undefined
+    // })
+
+    it('should be true', () => {
+        expect(true).toBe(true);
+    })
+
+})
