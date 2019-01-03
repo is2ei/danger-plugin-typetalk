2737a09a78b007d16775b388fa7c1922bd3f49df
Issei Horie <issei.horie@nulab-inc.com>
2019-01-03 23:23:36 +0900

    Fix lint error

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,74 +1,74 @@
-import { DangerResults } from "../node_modules/danger/distribution/dsl/DangerResults"
-import { buildEndpointURI, createMessage } from './'
+import { DangerResults } from "../node_modules/danger/distribution/dsl/DangerResults";
+import { createMessage } from "./";
 
-describe('postMessage()', () => {
+describe("postMessage()", () => {
 
-    it('should throw error when url is blank', () => {
+    it("should throw error when url is blank", () => {
         expect(() => {
-            postMessage('', 'message')
-        }).toThrow()
-    })
+            postMessage("", "message");
+        }).toThrow();
+    });
 
-    it('should throw error when url is not valid', () => {
+    it("should throw error when url is not valid", () => {
         expect(() => {
-            postMessage('abcde', 'message')
-        }).toThrow()
-    })
+            postMessage("abcde", "message");
+        }).toThrow();
+    });
 
-    it('should throw error when message is blank', () => {
+    it("should throw error when message is blank", () => {
         expect(() => {
-            postMessage('http://example.com', '')
-        }).toThrow()
-    })
-})
+            postMessage("http://example.com", "");
+        }).toThrow();
+    });
+});
 
-describe('createMessage()', () => {
+describe("createMessage()", () => {
 
     const emptyResults: DangerResults = {
         fails: [],
-        warnings: [],
-        messages: [],
         markdowns: [],
-    }
+        messages: [],
+        warnings: [],
+    };
 
-    it('should handle empty result', () => {
-        const message = createMessage(emptyResults)
-        expect(message).toBe(':white_check_mark: There is nothing to report...')
-    })
+    it("should handle empty result", () => {
+        const message = createMessage(emptyResults);
+        expect(message).toBe(":white_check_mark: There is nothing to report...");
+    });
 
     const failResults: DangerResults = {
-        fails: [ { message: 'Failure!!!', file: 'index.ts', line: 54321 } ],
-        warnings: [],
-        messages: [],
+        fails: [ { message: "Failure!!!", file: "index.ts", line: 54321 } ],
         markdowns: [],
-    }
+        messages: [],
+        warnings: [],
+    };
 
-    it('should handle failure result', () => {
-        const message = createMessage(failResults)
-        expect(message).toBe(`:rage: Failure\n• Failure!!! at index.ts line 54321`)
-    })
+    it("should handle failure result", () => {
+        const message = createMessage(failResults);
+        expect(message).toBe(":rage: Failure\n• Failure!!! at index.ts line 54321");
+    });
 
     const warnResults: DangerResults = {
         fails: [],
-        warnings: [ { message: 'Warning!!!', file: 'index.ts', line: 54321 } ],
-        messages: [],
         markdowns: [],
-    }
+        messages: [],
+        warnings: [ { message: "Warning!!!", file: "index.ts", line: 54321 } ],
+    };
 
-    it('should handle warning result', () => {
-        const message = createMessage(warnResults)
-        expect(message).toBe(`:angry: Warning\n• Warning!!! at index.ts line 54321`)
-    })
+    it("should handle warning result", () => {
+        const message = createMessage(warnResults);
+        expect(message).toBe(`:angry: Warning\n• Warning!!! at index.ts line 54321`);
+    });
 
     const messageResults: DangerResults = {
         fails: [],
-        warnings: [],
-        messages: [ { message: 'Hello World!!!', file: 'index.ts', line: 54321 } ],
         markdowns: [],
-    }
+        messages: [ { message: "Hello World!!!", file: "index.ts", line: 54321 } ],
+        warnings: [],
+    };
 
-    it('should handle message result', () => {
-        const message = createMessage(messageResults)
-        expect(message).toBe(`:open_mouth: Message\n• Hello World!!! at index.ts line 54321`)
-    })
-})
+    it("should handle message result", () => {
+        const message = createMessage(messageResults);
+        expect(message).toBe(":open_mouth: Message\n• Hello World!!! at index.ts line 54321");
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,121 +1,120 @@
-import axios from'axios' 
-import { DangerResults } from "../node_modules/danger/distribution/dsl/DangerResults"
+import axios from "axios";
+import { DangerResults } from "../node_modules/danger/distribution/dsl/DangerResults";
 
 interface PostMessageRequestParam {
-    message: string
+    message: string;
 }
 
 /**
  * Options to run typetalk()
  */
 export interface TypetalkOptions {
-    url: string
+    url: string;
 }
 
-const typetalkBaseURL: string = 'https://typetalk.com/'
+const typetalkBaseURL: string = "https://typetalk.com/";
 
 /**
  * Typetalk API doc: https://developer.nulab-inc.com/docs/typetalk/api/1/post-message/
  */
-const postMessageEndpintURI: string = '/api/v1/topics/'
+const postMessageEndpintURI: string = "/api/v1/topics/";
 
-declare var results: DangerResults
+declare var results: DangerResults;
 
 /**
  * Send report to Typetalk.
- * 
- * @param options 
+ *
+ * @param options
  */
 export default function typetalk(options: TypetalkOptions) {
-    const uri: string = buildEndpointURI()
-    const msg: string = createMessage(results)
-    postMessage(uri, msg)
+    const uri: string = buildEndpointURI();
+    const msg: string = createMessage(results);
+    postMessage(uri, msg);
 }
 
 /**
  * Build endpoint URL for postMessage.
  */
 export function buildEndpointURI(): string {
-    const topicID = process.env.TOPIC_ID
+    const topicID = process.env.TOPIC_ID;
 
-    if (topicID === undefined || topicID === '') {
-        throw Error(`'topic id' missing - must supply Typetalk topic id`)
+    if (topicID === undefined || topicID === "") {
+        throw Error(`'topic id' missing - must supply Typetalk topic id`);
     }
 
-    return postMessageEndpintURI + topicID
+    return postMessageEndpintURI + topicID;
 }
 
 /**
  * Create message from danger results.
- * 
+ *
  * @param r danger result
  */
 export function createMessage(r: DangerResults): string {
 
-    const fails     = r.fails
-    const warnings  = r.warnings
-    const messages  = r.messages
+    const fails     = r.fails;
+    const warnings  = r.warnings;
+    const messages  = r.messages;
 
     if (fails.length === 0 && warnings.length === 0  && messages.length === 0) {
-        return ':white_check_mark: There is nothing to report...'
+        return ":white_check_mark: There is nothing to report...";
     }
 
-    let message: string = ''
+    let message: string = "";
 
     if (fails.length > 0) {
-        message += ":rage: Failure\n" + fails.map(f => `• ${f.message} at ${f.file} line ${f.line}`).join("\n")
+        message += ":rage: Failure\n" + fails.map((f) => `• ${f.message} at ${f.file} line ${f.line}`).join("\n");
     }
     if (warnings.length > 0) {
-        message += ":angry: Warning\n" + warnings.map(w => `• ${w.message} at ${w.file} line ${w.line}`).join("\n")
+        message += ":angry: Warning\n" + warnings.map((w) => `• ${w.message} at ${w.file} line ${w.line}`).join("\n");
     }
     if (messages.length > 0) {
-        message += ":open_mouth: Message\n" + messages.map(m => `• ${m.message} at ${m.file} line ${m.line}`).join("\n")
+        message += ":open_mouth: Message\n" ;
+        message += messages.map((m) => `• ${m.message} at ${m.file} line ${m.line}`).join("\n");
     }
 
-    return message
+    return message;
 }
 
 /**
  * Post message to Typetalk
- * 
+ *
  * @param maybeURL post message endpoint URL
  * @param message message to post
  */
 export function postMessage(url: string, message: string): void {
 
-    if (url === undefined || url === '') {
-        throw Error(`'url' missing - must supply valid URL`)
+    if (url === undefined || url === "") {
+        throw Error(`'url' missing - must supply valid URL`);
     }
 
-    if (message === undefined || message === '') {
-        throw Error(`'message' missing - must supply message to post`)
+    if (message === undefined || message === "") {
+        throw Error(`'message' missing - must supply message to post`);
     }
 
-    const token = process.env.TYPETALK_TOKEN
-    if (token === undefined || token === '') {
-        throw Error(`'token' missing - must supply Typetalk topic id`)
+    const token = process.env.TYPETALK_TOKEN;
+    if (token === undefined || token === "") {
+        throw Error(`'token' missing - must supply Typetalk token`);
     }
 
-    let a = axios.create({
+    const a = axios.create({
         baseURL: typetalkBaseURL,
         headers: {
-            'Content-Type': 'application/json',
-            'X-Typetalk-Token': token,
+            "Content-Type": "application/json",
+            "X-Typetalk-Token": token,
         },
-        responseType: 'json',
         maxRedirects: 0,
         proxy: false,
-    })
+        responseType: "json",
+    });
 
-    let p: PostMessageRequestParam = {
-        message: message
-    }
-    
+    const p: PostMessageRequestParam = {message};
+
     a.post(url, p)
-    .then(function (response) {
+    .then((response) => {
         // Do something
     })
-    .catch(function (error) {
+    .catch((error) => {
         // Do something
-    })
+    });
 }
